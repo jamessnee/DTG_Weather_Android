746280bd9ab9c407b5555cf12d2a8c1c8c8c0e48
James Snee <james.snee@gmail.com>
2013-03-07 23:33:22 +0000

    Implemented the basic skeleton of the UI.

--- src/uk/ac/cam/dtg/weather/CurrentWeather.java
+++ src/uk/ac/cam/dtg/weather/CurrentWeather.java
@@ -4,30 +4,77 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 import uk.ac.cam.dtg.weather.connections.*;
+import uk.ac.cam.dtg.weather.models.Weather;
 
 public class CurrentWeather extends Activity {
+	
+	private Weather currWeather;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_current_weather);
 		
-		new AsyncTask<Void, Void, String>() {
-
-			@Override
-			protected String doInBackground(Void... params) {
-				DTGWSConnection.getCurrentWeather();
-				return "";
-			}
-		}.execute();
+		WeatherUpdater updater = new WeatherUpdater(this);
+		updater.execute();
+	}
+	
+	private void updateUI(){
+		TextView temp = (TextView) findViewById(R.id.temp_id);
+		temp.setText(currWeather.getTemp() + "\u00B0C");
+		
+		TextView humid = (TextView) findViewById(R.id.humid_id);
+		humid.setText(currWeather.getHumidity()+"%");
+		
+		TextView rain = (TextView) findViewById(R.id.rain_id);
+		rain.setText(currWeather.getRain() + "mm");
+		
+		TextView dew = (TextView) findViewById(R.id.dew_id);
+		dew.setText(currWeather.getDewPoint() + "\u00B0C");
+		
+		TextView wind = (TextView) findViewById(R.id.wind_id);
+		wind.setText(currWeather.getWindSpeed() + "kts");
+		
+		TextView pressure = (TextView) findViewById(R.id.pressure_id);
+		pressure.setText(currWeather.getPressure() + "mBar");
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.current_weather, menu);
 		return true;
+	}
+	
+	public Weather getCurrWeather() {
+		return currWeather;
+	}
+
+	public void setCurrWeather(Weather currWeather) {
+		this.currWeather = currWeather;
+	}
+	
+	private class WeatherUpdater extends AsyncTask<Void,Void,Weather>{
+		
+		private CurrentWeather weatherActivity;
+		
+		public WeatherUpdater(CurrentWeather cw){
+			this.weatherActivity = cw;
+		}
+		
+		@Override
+		protected Weather doInBackground(Void... params) {
+			Weather weather = DTGWSConnection.getCurrentWeather();
+			return weather;
+		}
+		
+		@Override
+		protected void onPostExecute(Weather weather){
+			weatherActivity.setCurrWeather(weather);
+			weatherActivity.updateUI();
+		}
+		
 	}
 
 }
