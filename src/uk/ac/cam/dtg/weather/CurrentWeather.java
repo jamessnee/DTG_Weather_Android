dabbf73a45ed36b6edbe05611cf9b6983738c696
James Snee <james.snee@gmail.com>
2013-07-22 16:38:47 +0100

    Handler and thread to manage background UI updates

--- src/uk/ac/cam/dtg/weather/CurrentWeather.java
+++ src/uk/ac/cam/dtg/weather/CurrentWeather.java
@@ -1,8 +1,9 @@
 package uk.ac.cam.dtg.weather;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.os.Handler;
+import android.os.Message;
 import android.view.Menu;
 import android.widget.TextView;
 import uk.ac.cam.dtg.weather.connections.*;
@@ -12,15 +13,56 @@ public class CurrentWeather extends Activity {
 	
 	private Weather currWeather;
 
+    /*
+     * This is where we receive notifications to update the UI
+     *
+     */
+    Handler handler = new Handler() {
+       @Override
+        public void handleMessage(Message message) {
+           if(message.what == 0) {
+               TextView temp = (TextView) findViewById(R.id.temp_id);
+               temp.setText(currWeather.getTemp() + "\u00B0C");
+
+               TextView humid = (TextView) findViewById(R.id.humid_id);
+               humid.setText(currWeather.getHumidity()+"%");
+
+               TextView rain = (TextView) findViewById(R.id.rain_id);
+               rain.setText(currWeather.getRain() + "mm");
+
+               TextView dew = (TextView) findViewById(R.id.dew_id);
+               dew.setText(currWeather.getDewPoint() + "\u00B0C");
+
+               TextView wind = (TextView) findViewById(R.id.wind_id);
+               wind.setText(currWeather.getWindSpeed() + "kts");
+
+               TextView pressure = (TextView) findViewById(R.id.pressure_id);
+               pressure.setText(currWeather.getPressure() + "mBar");
+           }
+       }
+    };
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_current_weather);
-		
-		WeatherUpdater updater = new WeatherUpdater(this);
-		updater.execute();
+
+        startUpdates();
 	}
-	
+
+    public void startUpdates() {
+        Runnable run = new Runnable() {
+            @Override
+            public void run() {
+                setCurrWeather(DTGWSConnection.getCurrentWeather());
+                handler.sendEmptyMessage(0);
+            }
+        };
+        Thread t = new Thread(run);
+        t.start();
+    }
+
+    /*
 	private void updateUI(){
 		TextView temp = (TextView) findViewById(R.id.temp_id);
 		temp.setText(currWeather.getTemp() + "\u00B0C");
@@ -40,6 +82,7 @@ public class CurrentWeather extends Activity {
 		TextView pressure = (TextView) findViewById(R.id.pressure_id);
 		pressure.setText(currWeather.getPressure() + "mBar");
 	}
+	*/
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -54,27 +97,4 @@ public class CurrentWeather extends Activity {
 	public void setCurrWeather(Weather currWeather) {
 		this.currWeather = currWeather;
 	}
-	
-	private class WeatherUpdater extends AsyncTask<Void,Void,Weather>{
-		
-		private CurrentWeather weatherActivity;
-		
-		public WeatherUpdater(CurrentWeather cw){
-			this.weatherActivity = cw;
-		}
-		
-		@Override
-		protected Weather doInBackground(Void... params) {
-			Weather weather = DTGWSConnection.getCurrentWeather();
-			return weather;
-		}
-		
-		@Override
-		protected void onPostExecute(Weather weather){
-			weatherActivity.setCurrWeather(weather);
-			weatherActivity.updateUI();
-		}
-		
-	}
-
 }
